49b7f846f2df3b0639db70eb77bd56922399ffe4
ogheneruno <orunosaduwa@gmail.com>
2019-08-20 14:33:52 +0100

    added react-router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component, Fragment } from 'react';
 import NavBar from '../src/components/UiComponents/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './components/HomePage/HomePage';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
-
 	render() {
 		return (
 			<Fragment>
-				<NavBar/>
-				<HomePage />
+				<NavBar />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+				</Switch>
 			</Fragment>
 		);
 	}
